3406f1b3664616c25b3d3223a778e53abb78e3a2
stonedch <stch.max.muhin@gmail.com>
2024-08-05 12:04:15 +0300

    chore: clear debug command

--- app/app/Console/Commands/DebugCommand.php
+++ app/app/Console/Commands/DebugCommand.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Helpers\FieldGetter;
-use App\Models\Departament;
-use App\Models\FormCheckerResult;
 use Illuminate\Console\Command;
 
 class DebugCommand extends Command
@@ -18,15 +15,5 @@ class DebugCommand extends Command
     // Please clear me after debug
     public function handle(): void
     {
-        FormCheckerResult::all()->map(function (FormCheckerResult $result) {
-            $result->delete();
-        });
-        // $departament = Departament::find(210);
-
-        // FieldGetter::find(
-        //     $departament,
-        //     [69, 67, 64, 63, 46, 44, 40, 33, 29, 25],
-        //     ['Число работников', 'Техническое обеспечение']
-        // );
     }
 }
