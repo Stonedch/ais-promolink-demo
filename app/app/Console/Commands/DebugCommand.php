e5b9bd9a185a9e4e9d595fea42481c21af36126b
stonedch <stch.max.muhin@gmail.com>
2024-09-04 20:27:56 +0300

    feat: add import users command

--- app/app/Console/Commands/DebugCommand.php
+++ app/app/Console/Commands/DebugCommand.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Helpers\PhoneNormalizer;
-use App\Models\User;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Hash;
-use Orchid\Platform\Models\Role;
 use Throwable;
 
 class DebugCommand extends Command
@@ -20,69 +16,5 @@ class DebugCommand extends Command
     // Please clear me after debug
     public function handle(): void
     {
-        $filepath = storage_path('/app/needed-users.csv');
-        $file = fopen($filepath, 'r');
-
-        $usersFilepath = storage_path('/app/users.csv');
-        $usersFile = fopen($usersFilepath, 'a+');
-        fputs($usersFile, chr(0xEF) . chr(0xBB) . chr(0xBF));
-
-        $roles = Role::where('slug', 'ILIKE', 'user')->pluck('id')->toArray();
-
-        try {
-            $index = 0;
-
-            fputcsv($usersFile, [
-                'Фамилия',
-                'Имя',
-                'Отчество',
-                'Номер телефона',
-                'Пароль',
-                'E-mail',
-            ], ';');
-
-            while ($line = fgetcsv($file, null, '	')) {
-                if (empty($line)) break;
-                if ($index++ <= 0) continue;
-
-                $phone = PhoneNormalizer::normalizePhone($line[5]);
-                $password = $line[6] ?: self::genPassword();
-
-                if (empty($phone)) continue;
-                
-                $userData = [
-                    'last_name' => $line[2],
-                    'first_name' => $line[3],
-                    'middle_name' => $line[4],
-                    'phone' => $phone,
-                    'password' => Hash::make($password),
-                ];
-
-                $user = User::where('phone', $phone)->first() ?: new User();
-                $user->fill($userData);
-                $user->save();
-
-                $user->replaceRoles($roles);
-
-                $userData['password'] = $password;
-
-                fputcsv($usersFile, $userData, ';');
-            }
-        } catch (Throwable $e) {
-            $this->error($e->getMessage());
-        } finally {
-            fclose($file);
-            fclose($usersFilepath);
-        }
-    }
-
-    private static function genPassword(): string
-    {
-        return str_replace(';', '%', \Illuminate\Support\Str::password(8));
-    }
-
-    private static function genEmail(string $phone): string
-    {
-        return "{$phone}@promo-link.ru";
     }
 }
---
+++ app/app/Console/Commands/ImportUsersCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Helpers\PhoneNormalizer;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Orchid\Platform\Models\Role;
+use Throwable;
+
+class ImportUsersCommand extends Command
+{
+    protected $name = 'import-users:run {fromCsv} {toCsv}';
+    protected $signature = 'import-users:run {fromCsv} {toCsv}';
+    protected $description = 'import-users:run {fromCsv} {toCsv}';
+
+    public function handle(): void
+    {
+        $filepath = storage_path($this->argument('fromCsv'));
+        $file = fopen($filepath, 'r');
+
+        $usersFilepath = storage_path($this->argument('toCsv'));
+        $usersFile = fopen($usersFilepath, 'a+');
+        fputs($usersFile, chr(0xEF) . chr(0xBB) . chr(0xBF));
+
+        $roles = Role::where('slug', 'ILIKE', 'user')->pluck('id')->toArray();
+
+        try {
+            $index = 0;
+
+            fputcsv($usersFile, [
+                'Фамилия',
+                'Имя',
+                'Отчество',
+                'Номер телефона',
+                'Пароль',
+                'E-mail',
+            ], ';');
+
+            while ($line = fgetcsv($file, null, '	')) {
+                if (empty($line)) break;
+                if ($index++ <= 0) continue;
+
+                $phone = PhoneNormalizer::normalizePhone($line[5]);
+                $password = $line[6] ?: self::genPassword();
+
+                if (empty($phone)) continue;
+                
+                $userData = [
+                    'last_name' => $line[2],
+                    'first_name' => $line[3],
+                    'middle_name' => $line[4],
+                    'phone' => $phone,
+                    'password' => Hash::make($password),
+                ];
+
+                $user = User::where('phone', $phone)->first() ?: new User();
+                $user->fill($userData);
+                $user->save();
+
+                $user->replaceRoles($roles);
+
+                $userData['password'] = $password;
+
+                fputcsv($usersFile, $userData, ';');
+            }
+        } catch (Throwable $e) {
+            $this->error($e->getMessage());
+        } finally {
+            fclose($file);
+            fclose($usersFilepath);
+        }
+    }
+
+    private static function genPassword(): string
+    {
+        return str_replace(';', '%', \Illuminate\Support\Str::password(8));
+    }
+}
