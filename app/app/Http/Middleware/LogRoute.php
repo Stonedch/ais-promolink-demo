d42bb62adcdd628f8705b1bd076aed1324cbb2fa
stonedch <stch.max.muhin@gmail.com>
2024-09-11 17:47:53 +0300

    feat: daily log

---
+++ app/app/Http/Middleware/LogRoute.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class LogRoute
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        try {
+            $log = [
+                'status' => $response->status(),
+                'user' => $request->user() ?: null,
+                'uri' => $request->getUri(),
+                'method' => $request->getMethod(),
+                'request_body' => $request->all(),
+            ];
+
+            Log::info(json_encode($log, JSON_UNESCAPED_UNICODE));
+        } catch (Throwable) {
+        }
+
+        return $response;
+    }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Middleware\LogRoute;
 use Illuminate\Support\Facades\Route;
 
 // web
-Route::name('web.')->group(function () {
+Route::middleware([LogRoute::class])->name('web.')->group(function () {
     // web.index
     Route::name('index.')->prefix('/')->controller(\App\Http\Controllers\Web\IndexController::class)->group(function () {
         Route::get('/', 'index')->name('index');
@@ -60,7 +61,7 @@ Route::name('web.')->group(function () {
 });
 
 // api
-Route::name('api.')->prefix('/api')->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])->group(function () {
+Route::middleware([LogRoute::class])->name('api.')->prefix('/api')->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])->group(function () {
     // api.auth
     Route::name('auth.')->prefix('/auth')->group(function () {
         //api.auth.login
