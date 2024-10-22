b94793af3ffe9ef6a96ca34e0b73d08ee138edb0
Stonedch <stch.max.muhin@gmail.com>
2024-10-22 09:56:35 +0000

    fix: вынес ссылку на дашборд в настройки

--- app/config/content.php
+++ app/config/content.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'datalens' => [
+        'url' => env('CONTENT_DATALENS_URL', null),
+    ],
+
     'minister' => [
         'reports' => [
             'title' => env('CONTENT_MINISTER_REPORTS_TITLE', 'Подведомственные учреждения'),
