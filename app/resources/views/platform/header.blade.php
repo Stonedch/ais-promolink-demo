200d110696c7533d565f126dce1a7554c33ef261
promolinkru <se@promo-link.ru>
2024-11-14 15:29:32 +0000

    fix: поправил баг с подгрузкой превью

--- app/resources/views/platform/header.blade.php
+++ app/resources/views/platform/header.blade.php
@@ -1,10 +1,11 @@
-<script src="/plugins/jquery.min.js"></script>
-<script src="/plugins/jquery-ui-sortable/jquery-ui.js"></script>
-<link rel="stylesheet" href="/plugins/jquery-ui-sortable/jquery-ui.css">
-<script src="/js/lodash.js"></script>
-<script src="/js/orchid.js"></script>
-<script src="/js/base.js"></script>
-<script src="/js/interface.js"></script>
-<script src="/plugins/fancyapps/ui/dist/index.umd.js"></script>
-<link rel="stylesheet" href="/plugins/fancyapps/ui/dist/fancybox/fancybox.css" />
-<link rel="stylesheet" href="/css/orchid.css">
+@php $headerBladeImportsVersion = 2; @endphp
+<script src="/plugins/jquery.min.js?version={{ $headerBladeImportsVersion }}"></script>
+<script src="/plugins/jquery-ui-sortable/jquery-ui.js?version={{ $headerBladeImportsVersion }}"></script>
+<link rel="stylesheet" href="/plugins/jquery-ui-sortable/jquery-ui.css?version={{ $headerBladeImportsVersion }}">
+<script src="/js/lodash.js?version={{ $headerBladeImportsVersion }}"></script>
+<script src="/js/orchid.js?version={{ $headerBladeImportsVersion }}"></script>
+<script src="/js/base.js?version={{ $headerBladeImportsVersion }}"></script>
+<script src="/js/interface.js?version={{ $headerBladeImportsVersion }}"></script>
+<script src="/plugins/fancyapps/ui/dist/index.umd.js?version={{ $headerBladeImportsVersion }}"></script>
+<link rel="stylesheet" href="/plugins/fancyapps/ui/dist/fancybox/fancybox.css?version={{ $headerBladeImportsVersion }}" />
+<link rel="stylesheet" href="/css/orchid.css?version={{ $headerBladeImportsVersion }}">
