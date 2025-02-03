8eb0239e0135d51b447c97112676bfa56b04d48e
stonedch <stch.max.muhin@gmail.com>
2025-02-03 20:54:08 +0300

    chore: minor changes to event archive

--- app/public/js/pl-assets/pl-form-archive.js
+++ app/public/js/pl-assets/pl-form-archive.js
@@ -17,15 +17,19 @@ export class PLFormArchive {
                         var rows = "";
 
                         _.each(response.data.events, (event) => {
+                            const structure = JSON.parse(event.form_structure);
                             rows += /* HTML */ `
                                 <tr>
-                                    <td class="w-50">${event.filled_at}</td>
-                                    <td class="w-50">
+                                    <td class="w-50 text-nowrap pt-4"><b>${structure.form.name}</b></td>
+                                    <td class="w-25 text-nowrap pt-4">${event.filled_at}</td>
+                                    <td class="w-25">
                                         <a href="/forms/preview/${event.departament_id}/${event.form_id}?event=${event.id}"
                                             data-bs-toggle="tooltip"
                                             data-bs-placement="bottom"
-                                            title="Просмотр">
+                                            title="Просмотр"
+                                            class="btn btn-ligth d-flex gap-3">
                                             <img class="_icon-link" width="20px" src="/img/search.svg" />
+                                            <span>Просмотр</span>
                                         </a>
                                     </td>
                                 </tr>
@@ -36,8 +40,9 @@ export class PLFormArchive {
                             <table class="table m-0 w-auto">
                                 <thead>
                                     <tr>
-                                        <th scope="col">Дата заполнения</th>
-                                        <th scope="col">Взаимодействия</th>
+                                        <th class="w-50 text-nowrap" scope="col">Название</th>
+                                        <th class="w-25 text-nowrap" scope="col">Дата заполнения</th>
+                                        <th class="w-25 text-nowrap" scope="col">Взаимодействия</th>
                                     </tr>
                                 </thead>
                                 <tbody>
