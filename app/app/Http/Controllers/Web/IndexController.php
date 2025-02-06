5a3ed48b37d6dd22b088daa70fb912b535960a19
stonedch <stch.max.muhin@gmail.com>
2025-02-06 14:22:04 +0300

    fix: поправил передачу ошибок при redirect

--- app/app/Http/Controllers/Web/IndexController.php
+++ app/app/Http/Controllers/Web/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\ViewErrorBag;
 use Throwable;
 
 class IndexController extends Controller
@@ -18,7 +19,11 @@ class IndexController extends Controller
                 new AuthorizationException('Для продолжения необходимо авторизоваться!')
             );
 
-            return redirect()->route('web.home.index');
+            $errors = session()->get('errors', app(ViewErrorBag::class));
+
+            return redirect()
+                ->route('web.home.index')
+                ->withErrors(@$errors->getBag('default')->all());
         } catch (AuthorizationException $e) {
             return redirect()
                 ->route('web.auth.login.index')
