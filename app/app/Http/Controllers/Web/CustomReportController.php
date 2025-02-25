bdcf3f925ca19fcc6b8e7b3dafc87fd9a91145dc
stonedch <stch.max.muhin@gmail.com>
2025-02-25 10:03:49 +0300

    feat: добавил лог загруженных файлов для пользователей

--- app/app/Http/Controllers/Web/CustomReportController.php
+++ app/app/Http/Controllers/Web/CustomReportController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Web;
 
 use App\Exceptions\HumanException;
 use App\Http\Controllers\Controller;
+use App\Models\CustomReport;
+use App\Models\CustomReportLog;
+use App\Models\CustomReportType;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -29,7 +32,37 @@ class CustomReportController extends Controller
                 new HumanException('Ошибка доступа к функционалу кастомного отчета!')
             );
 
-            return view($this->views['index']);
+            $response = [];
+
+            $response['logs'] = CustomReportLog::query()
+                ->where('user_id', $user->id)
+                ->whereNotNull('custom_report_type_id')
+                ->whereNotNull('custom_report_id')
+                ->orderBy('created_at', 'DESC')
+                ->get()
+                ->map(function (CustomReportLog $log) {
+                    if ($log->message == 'loaded') {
+                        $log->message = 'Документ загружен';
+                    }
+
+                    if ($log->message == 'is ready') {
+                        $log->message = 'Документ полностью обработан';
+                    }
+
+                    return $log;
+                });
+
+            $response['customReportTypes'] = CustomReportType::query()
+                ->whereIn('id', $response['logs']->pluck('custom_report_type_id'))
+                ->get()
+                ->keyBy('id');
+
+            $response['customReports'] = CustomReport::query()
+                ->whereIn('id', $response['logs']->pluck('custom_report_id'))
+                ->get()
+                ->keyBy('id');
+
+            return view($this->views['index'], $response);
         } catch (HumanException $e) {
             return redirect()
                 ->route('web.index.index')
