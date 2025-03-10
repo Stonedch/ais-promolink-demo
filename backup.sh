34b1ebc1f96793dbfc78dfb9ea928207ba0e4015
stonedch <stch.max.muhin@gmail.com>
2025-03-10 12:43:42 +0300

    fix: поправил ошибку бэкапов из под cronа

--- backup.sh
+++ backup.sh
@@ -12,7 +12,7 @@ FILEPATH="${BACKUP_FOLDER}/${FILENAME}";
 mkdir -p ${BACKUP_ROOT_FOLDER}
 mkdir -p ${BACKUP_FOLDER};
 
-${DOCKER_COMPOSE_COMMAND:-docker-compose} exec db pg_dump -U ${DB_USERNAME} ${DB_DATABASE} > ${FILEPATH};
+${DOCKER_COMPOSE_COMMAND:-docker-compose} exec -T db pg_dump -U ${DB_USERNAME} ${DB_DATABASE} > ${FILEPATH};
 
 if ${FTP_BACKUP_AVAILABLE}
     then curl -T ${FILEPATH} --ftp-create-dirs -u ${FTP_BACKUP_USERNAME}:${FTP_BACKUP_PASSWORD} ${FTP_BACKUP_HOST}
