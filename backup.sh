924671bd49d97d44f80a42b4ca304f653c0d9970
stonedch <stch.max.muhin@gmail.com>
2025-03-26 15:47:05 +0300

    chore: разделил выгрузки данных и схемы

--- backup.sh
+++ backup.sh
@@ -6,14 +6,19 @@ TIMESTAMP="$(date +%Y-%m-%d-%H-%m-%s)";
 
 BACKUP_FOLDER="${BACKUP_ROOT_FOLDER}/backup-${TIMESTAMP}";
 
-FILENAME="dump_${TIMESTAMP}.sql";
-FILEPATH="${BACKUP_FOLDER}/${FILENAME}";
+INSERTS_FILENAME="inserts-${TIMESTAMP}.sql";
+INSERTS_FILEPATH="${BACKUP_FOLDER}/${INSERTS_FILENAME}";
+
+SCHEMA_FILENAME="schema-${TIMESTAMP}.sql";
+SCHEMA_FILEPATH="${BACKUP_FOLDER}/${SCHEMA_FILENAME}";
 
 mkdir -p ${BACKUP_ROOT_FOLDER}
 mkdir -p ${BACKUP_FOLDER};
 
-${DOCKER_COMPOSE_COMMAND:-docker-compose} exec -T db pg_dump -U ${DB_USERNAME} ${DB_DATABASE} > ${FILEPATH};
+${DOCKER_COMPOSE_COMMAND:-docker-compose} exec -T db pg_dump -a --inserts --attribute-inserts -U ${DB_USERNAME} ${DB_DATABASE} > ${INSERTS_FILEPATH};
+${DOCKER_COMPOSE_COMMAND:-docker-compose} exec -T db pg_dump --schema-only -U ${DB_USERNAME} ${DB_DATABASE} > ${SCHEMA_FILEPATH};
 
-if ${FTP_BACKUP_AVAILABLE}
-    then curl -T ${FILEPATH} --ftp-create-dirs -u ${FTP_BACKUP_USERNAME}:${FTP_BACKUP_PASSWORD} ${FTP_BACKUP_HOST}
+if ${FTP_BACKUP_AVAILABLE}; then
+    curl -T ${INSERTS_FILEPATH} --ftp-create-dirs -u ${FTP_BACKUP_USERNAME}:${FTP_BACKUP_PASSWORD} ${FTP_BACKUP_HOST};
+    curl -T ${SCHEMA_FILEPATH} --ftp-create-dirs -u ${FTP_BACKUP_USERNAME}:${FTP_BACKUP_PASSWORD} ${FTP_BACKUP_HOST};
 fi
